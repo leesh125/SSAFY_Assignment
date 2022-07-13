ceb9bbe979218aaf52fc31b64c7465711f0343bd
SSAFY.Hong <iccack70@gamil.com>
2022-07-13 22:28:03 +0900

    java_ws_03_3 upload

---
+++ src/com/ssafy/ws/step3/BuildingTest.java
@@ -0,0 +1,13 @@
+package com.ssafy.ws.step3;
+
+/**
+ * B구획의 빌딩 최고 높이 구하기
+ */ 
+public class BuildingTest {
+
+	public static void main(String[] args) throws Exception{
+		// 코드를 작성해주세요.
+		
+		
+	}
+}
